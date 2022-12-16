02beebaf78d16517207d809fbdcaa6bf0edca854
Mustafa Arslan <marslanahmet@hotmail.com>
2022-12-16 21:07:59 +0300

    ForgotPassword Added

--- CurrencyBank/CurrencyBank/Controllers/UserController.cs
+++ CurrencyBank/CurrencyBank/Controllers/UserController.cs
@@ -53,5 +53,53 @@ namespace CurrencyBank.Controllers
                 Message = "User Registered"
             });
         }
+
+        [HttpPost("ForgotPassword")]
+        public async Task<IActionResult> ForgotPassword([FromBody] string Email)
+        {
+            if (Email == null)
+            {
+                return BadRequest();
+            }
+
+            var User = _AuthContext.Users.FirstOrDefault(u=>u.Email == Email);
+            if (User == null)
+            {
+                return NotFound();
+            }
+
+            User.Password = CreatePassword();
+            await _AuthContext.SaveChangesAsync();
+
+            return Ok(new
+            {
+                Message = "Password Changed"
+            });
+        }
+
+
+        private string CreatePassword()
+        {
+            Random rnd = new Random();
+            int randomnumber = rnd.Next(0, 11);
+            int randomchars = rnd.Next(0, 29);
+            int randomspecialcharacters = rnd.Next(0, 6);
+
+            int randomnumber2 = rnd.Next(0, 11);
+            int randomchars2 = rnd.Next(0, 29);
+            int randomspecialcharacters2 = rnd.Next(0, 6);
+
+            int randomnumber3 = rnd.Next(0, 11);
+            int randomchars3 = rnd.Next(0, 29);
+            int randomspecialcharacters3 = rnd.Next(0, 6);
+
+            string numbers = "0123456789";
+            string Chars = "abcçdefgğhıijklmnoöprsştuüvyz";
+            string specialcharacters = "@$!%&?";
+
+            return "" + numbers[randomnumber] + Chars[randomchars] + specialcharacters[randomspecialcharacters] +
+                   numbers[randomnumber2] + Chars[randomchars2] + specialcharacters[randomspecialcharacters2] +
+                   numbers[randomnumber3] + Chars[randomchars3] + specialcharacters[randomspecialcharacters3];
+        }
     }
 }
